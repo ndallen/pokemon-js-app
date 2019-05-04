d57739f6a936e31d659a98f3cf0f2ff907bc2f5f
ndallen <ecommerceevolution@gmail.com>
2019-05-04 21:35:00 +0100

    added conditional for height above 5 to JS code

--- pokemon-js-app/js/script.js
+++ pokemon-js-app/js/script.js
@@ -1,21 +1,26 @@
 var repository = [
     {
     name:'Bulbasaur',
-    height:'2\" 04',
+    height: 2.4,
     types: ['grass', 'poison']
     },
     {
     name:'Charizard',
-    height:'5\" 07',
+    height: 5.7,
     types: ['fire', 'flying']
     },
     {
     name:'Butterfree',
-    height:'3\" 07',
+    height: 3.7,
     types: ['bug', 'flying']
         },
 ];
 
 for(var i = 0; i < repository.length ; i++) {
- document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height);
+    if(repository[i].height > 5.0) {
+        document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height + ' feet. Wow thats big...');
+    } else {
+        document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height + ' feet.');
+    }
+ 
 }
