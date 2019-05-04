4d89292ee0d2b88190c7d1b7550e7cc7a32c38c6
ndallen <ecommerceevolution@gmail.com>
2019-05-04 20:34:42 +0100

    added 3 pokemon charcter objects to array

--- pokemon-js-app/js/script.js
+++ pokemon-js-app/js/script.js
@@ -1,1 +1,19 @@
-alert('Hello World');
+var repository = [
+    {
+    name:'Bulbasaur',
+    height:'2\" 04',
+    types: ['grass', 'poison']
+    },
+    {
+    name:'Charizard',
+    height:'5\" 07',
+    types: ['fire', 'flying']
+    },
+    {
+    name:'Butterfree',
+    height:'3\" 07',
+    types: ['bug', 'flying']
+        },
+];
+
+document.write(repository.name);
