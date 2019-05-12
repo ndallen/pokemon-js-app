0243faae1b627dd612f37532dc7868d96ef15d8d
ndallen <ecommerceevolution@gmail.com>
2019-05-12 15:48:25 +0100

    worked IIFE functions and added foreach function. Issue with array

--- pokemon-js-app/js/script.js
+++ pokemon-js-app/js/script.js
@@ -1,26 +1,72 @@
-var repository = [
-    {
-    name:'Bulbasaur',
-    height: 2.4,
-    types: ['grass', 'poison']
-    },
-    {
-    name:'Charizard',
-    height: 5.7,
-    types: ['fire', 'flying']
-    },
-    {
-    name:'Butterfree',
-    height: 3.7,
-    types: ['bug', 'flying']
+var pokemonRepository = (function () {
+    var repository = [
+        {
+            name: 'Bulbasaur',
+            height: 2.4,
+            types: ['grass', 'poison']
         },
-];
+        {
+            name: 'Charizard',
+            height: 5.7,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Butterfree',
+            height: 3.7,
+            types: ['bug', 'flying']
+        },
 
-for(var i = 0; i < repository.length ; i++) {
-    if(repository[i].height > 5.0) {
-        document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height + ' feet. Wow thats big..');
-    } else {
-        document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height + ' feet.');
+    ];
+
+
+    function add(repository) {
+        pokemonRepository.push(repository);
     }
- 
-}
+    function getAll() {
+        return repository;
+    }
+
+    return {
+        add: function(add) {
+            repository.push(add);
+        },
+        getAll: function() {
+            return repository;
+        }
+    };
+    
+    // return {
+    //     add: add,
+    //     getAll: getAll
+    // };
+
+
+})();
+
+Object.keys(pokemonRepository).forEach(function (getAll) {
+    
+    if (pokemonRepository.getAll()[1].height > 5.0) {
+
+                document.write('<p>' + pokemonRepository.getAll()[1].name + '\'s height is: ' + pokemonRepository.getAll()[1].height + ' feet. Wow thats big..');
+            } else {
+             
+                document.write('<p>' + pokemonRepository.getAll()[1].name + '\'s height is: ' + pokemonRepository.getAll()[1].height + ' feet.');
+                    }
+
+    // console.log(pokemonRepository.getAll()[0].name)
+    // console.log(pokemonRepository.getAll());
+});
+
+
+
+
+// OLD FOR LOOP CODE THAT WORKED
+
+    // for (var i = 0; i < repository.length; i++) {
+    //     if (repository[i].height > 5.0) {
+    //         document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height + ' feet. Wow thats big..');
+    //     } else {
+    //         document.write('<p>' + repository[i].name + '\'s height is: ' + repository[i].height + ' feet.');
+    //     }
+
+    // }
