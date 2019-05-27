9013fbaf4889764113d580f442a01b47d64cf7c4
ndallen <ecommerceevolution@gmail.com>
2019-05-27 11:46:13 +0100

    designed functions to display Pokemon Name after creating li in DOM. Name is output to screen

--- pokemon-js-app/js/script.js
+++ pokemon-js-app/js/script.js
@@ -2,90 +2,90 @@ var pokemonRepository = (function () {
     var repository = [
         {
             name: 'Bulbasaur',
-            pokemonId:1,
+            pokemonId: 1,
             height: 0.7,
-            weight:6.9,
-            species:'Seed Pokemon',
-            types: ['grass', 'poison'], 
-            abilities: 'Overgrow',         
-            image: src="https://img.pokemondb.net/artwork/bulbasaur.jpg"
+            weight: 6.9,
+            species: 'Seed Pokemon',
+            types: ['grass', 'poison'],
+            abilities: 'Overgrow',
+            image: src = "https://img.pokemondb.net/artwork/bulbasaur.jpg"
 
         },
         {
             name: 'Ivysaur',
-            pokemonId:2,
+            pokemonId: 2,
             height: 1.0,
-            weight:13.0,
-            species:'Seed Pokemon',
-            types: ['grass', 'poison'], 
-            abilities: 'Overgrow',            
-            image: src="https://img.pokemondb.net/artwork/ivysaur.jpg"
+            weight: 13.0,
+            species: 'Seed Pokemon',
+            types: ['grass', 'poison'],
+            abilities: 'Overgrow',
+            image: src = "https://img.pokemondb.net/artwork/ivysaur.jpg"
 
         },
         {
             name: 'Venusaur',
-            pokemonId:3,
+            pokemonId: 3,
             height: 2.0,
-            weight:100.0,
-            species:'Seed Pokemon',
-            types: ['grass', 'poison'], 
-            abilities: 'Overgrow',         
-            image: src="https://img.pokemondb.net/artwork/venusaur.jpg"
+            weight: 100.0,
+            species: 'Seed Pokemon',
+            types: ['grass', 'poison'],
+            abilities: 'Overgrow',
+            image: src = "https://img.pokemondb.net/artwork/venusaur.jpg"
 
         },
         {
             name: 'Charmander',
-            pokemonId:4,
+            pokemonId: 4,
             height: 0.6,
-            weight:8.5,
-            species:'Lizard Pokemon',
-            types: ['fire'], 
-            abilities: 'blaze',   
-            image: src="https://img.pokemondb.net/artwork/charmander.jpg"
+            weight: 8.5,
+            species: 'Lizard Pokemon',
+            types: ['fire'],
+            abilities: 'blaze',
+            image: src = "https://img.pokemondb.net/artwork/charmander.jpg"
 
         },
         {
             name: 'Charmeleon',
-            pokemonId:5,
+            pokemonId: 5,
             height: 1.1,
-            weight:19.0,
-            species:'Flame Pokemon',
-            types: ['fire'], 
-            abilities: 'blaze',         
-            image: src="https://img.pokemondb.net/artwork/charmeleon.jpg"
+            weight: 19.0,
+            species: 'Flame Pokemon',
+            types: ['fire'],
+            abilities: 'blaze',
+            image: src = "https://img.pokemondb.net/artwork/charmeleon.jpg"
 
         },
         {
             name: 'Charizard',
-            pokemonId:6,
+            pokemonId: 6,
             height: 1.7,
-            weight:90.5,
-            species:'Flame Pokemon',
-            types: ['Fire', 'Flying'], 
-            abilities:'blaze',    
-            image: src="https://img.pokemondb.net/artwork/charizard.jpg"
+            weight: 90.5,
+            species: 'Flame Pokemon',
+            types: ['Fire', 'Flying'],
+            abilities: 'blaze',
+            image: src = "https://img.pokemondb.net/artwork/charizard.jpg"
 
         },
         {
             name: 'Squirtle',
-            pokemonId:7,
+            pokemonId: 7,
             height: 0.5,
-            weight:9.0,
-            species:'Tiny Turtle Pokemon',
-            types: ['Water'], 
-            abilities:'torrent',    
-            image: src="https://img.pokemondb.net/artwork/squirtle.jpg"
+            weight: 9.0,
+            species: 'Tiny Turtle Pokemon',
+            types: ['Water'],
+            abilities: 'torrent',
+            image: src = "https://img.pokemondb.net/artwork/squirtle.jpg"
         },
         {
             name: 'Wartortle',
-            pokemonId:8,
+            pokemonId: 8,
             height: 1.0,
-            weight:22.5,
-            species:'Flame Pokemon',
-            types: ['Water'], 
-            abilities:'torrent',    
-            image: src="https://img.pokemondb.net/artwork/wartortle.jpg"
-        },
+            weight: 22.5,
+            species: 'Flame Pokemon',
+            types: ['Water'],
+            abilities: 'torrent',
+            image: src = "https://img.pokemondb.net/artwork/wartortle.jpg"
+        }
 
     ];
 
@@ -95,26 +95,36 @@ var pokemonRepository = (function () {
     function getAll() {
         return repository;
     }
+    //show pokemon function
+    function showDetails(pokemon) {
+        document.getElementById(pokemon.pokemonId)
+        var printInfo = "<p>" + pokemon.name + "</p>"
+        document.querySelector('.pokedex-output').innerHTML = printInfo
 
-    return {
-        add: function (add) {
-            repository.push(add);
-        },
-        getAll: function () {
-            return repository;
+
+    }
+    function addListItem(pokemon) {
+        var ul = document.getElementById("list");
+        var li = document.createElement("li");
+        li.classList.add('list-group-item');
+        li.setAttribute('id', pokemon.pokemonId);
+        li.onclick = function () {
+            return showDetails(pokemon);
         }
+        li.appendChild(document.createTextNode(pokemon.name));
+        ul.appendChild(li);
+    }
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
     };
 })();
 
-pokemonRepository.getAll().forEach(function (getAll) {
+// OUTPUT POKEMON NAME 
 
-    if (getAll.height > 1.0) {
-
-        document.write('<p>' + getAll.name + '\'s height is: ' + getAll.height + ' feet. Wow thats big..');
-    } else {
-
-        document.write('<p>' + getAll.name + '\'s height is: ' + getAll.height + ' feet.');
-    }
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
-
 
